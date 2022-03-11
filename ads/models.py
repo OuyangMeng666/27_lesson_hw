6935a8ed2623485a098ba6d34d7a5b3ece055a14
ouyang_meng <narelen@rambler.ru>
2022-03-11 21:45:44 +0300

    update

--- ads/models.py
+++ ads/models.py
@@ -9,7 +9,7 @@ class Ad(models.Model):
     name = models.CharField(max_length=20)
     author = models.CharField(max_length=50)
     price = models.PositiveIntegerField()
-    description = models.TextField(max_length=1000, null=True)
+    description = models.TextField(null=True)
     address = models.CharField(max_length=200)
     is_published = models.BooleanField(default=False)
 
--- ads/views.py
+++ ads/views.py
@@ -10,50 +10,6 @@ from django.views.generic import DetailView
 from ads.models import Category, Ad
 
 
-class add_info_ads(View):
-    def get(self, request):
-        data_ads = pandas.read_csv("S:/lesson27/new_project/data/ads.csv", sep=",").to_dict()
-
-        i = 0
-
-        while max(data_ads["Id"].keys()) > i:
-            print(data_ads["name"][i])
-            ad = Ad.objects.create(
-                name=data_ads["name"][i],
-                author=data_ads["author"][i],
-                price=data_ads["price"][i],
-                description=data_ads["description"][i],
-                address=data_ads["address"][i],
-                is_published=data_ads["is_published"][i]
-            )
-            i += 1
-
-        return JsonResponse("Done", safe=False, status=200)
-
-
-class add_info_cat(View):
-    def get(self, request):
-        data_ads = pandas.read_csv("S:/lesson27/new_project/data/categories.csv", sep=",").to_dict()
-
-        i = 0
-
-        while max(data_ads["id"].keys()) > i:
-            cat = Category.objects.create(
-                name=data_ads["name"][i],
-            )
-            i += 1
-
-        return JsonResponse("Done", safe=False, status=200)
-
-
-class AdDetailView1(DetailView):
-
-    def get(self, request, *args, **kwargs):
-        ad = self.get_object()
-
-        return JsonResponse(200, {"status": "ok"})
-
-
 @method_decorator(csrf_exempt, name='dispatch')
 class AdView(View):
     def get(self, request):
@@ -81,15 +37,7 @@ class AdView(View):
             is_published=ad_data["is_published"],
         )
 
-        return JsonResponse({
-            "id": ad.id,
-            "name": ad.name,
-            "author": ad.author,
-            "price": ad.price,
-            "description": ad.description,
-            "address": ad.address,
-            "is_published": ad.is_published,
-        })
+        return JsonResponse(status=201)
 
 
 class AdDetailViews(DetailView):
@@ -125,11 +73,10 @@ class CategoryView(View):
     def post(self, request):
         category_data = json.loads(request.body)
 
-        category = Category.objects.create(
+        Category.objects.create(
             name=category_data["name"],
         )
-        return JsonResponse({"id": category.id,
-                            "name": category.name,}, safe=False)
+        return JsonResponse(status=201)
 
 
 class CategoryDetailView(DetailView):
