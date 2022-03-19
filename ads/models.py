2f9ac32d2fe5efaa769e82cdb2a09c8cf92d8657
ouyang_meng <narelen@rambler.ru>
2022-03-19 22:25:30 +0300

    lesson 28 homework update

--- ads/models.py
+++ ads/models.py
@@ -1,15 +1,33 @@
 from django.db import models
 
+from users.models import User
+
 
 class Category(models.Model):
     name = models.CharField(max_length=20)
 
+    class Meta:
+        verbose_name = "Категория"
+        verbose_name_plural = "Категории"
+
+    def __str__(self):
+        return self.name
+
 
 class Ad(models.Model):
-    name = models.CharField(max_length=20)
-    author = models.CharField(max_length=50)
+    name = models.CharField(max_length=50)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
     price = models.PositiveIntegerField()
     description = models.TextField(null=True)
     address = models.CharField(max_length=200)
     is_published = models.BooleanField(default=False)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
+    #image = models.ImageField(upload_to="ads/", null=True, blank=True)
+
+    class Meta:
+        verbose_name = "Объявление"
+        verbose_name_plural = "Объявления"
+
+    def __str__(self):
+        return self.name
 
--- project/urls.py
+++ project/urls.py
@@ -1,15 +1,22 @@
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from ads import views
 
 urlpatterns = [
+    path('', views.root),
     path('admin/', admin.site.urls),
     path('cat/', views.CategoryView.as_view()),
-    path('add_info_ads', views.add_info_ads.as_view()),
-    path('add_info_cats', views.add_info_cat.as_view()),
     path('cat/<int:pk>', views.CategoryDetailView.as_view()),
+    path('cat/create/', views.CategoryCreateView.as_view()),
+    path('cat/<int:pk>/update/', views.CategoryUpdateView.as_view()),
+    path('cat/<int:pk>/delete/', views.CategoryDeleteView.as_view()),
     path('ad/', views.AdView.as_view()),
-    path('ad/<int:pk>', views.AdDetailViews.as_view()),
+    path('ad/<int:pk>', views.AdDetailView.as_view()),
+    path('ad/create', views.AdCreateView.as_view()),
+    path('ad/<int:pk>/update/', views.AdUpdateView.as_view()),
+    path('ad/<int:pk>/delete', views.AdDeleteView.as_view()),
+    path('ad/<int:pk>/update_image', views.AdUploadImageView.as_view()),
+    path('user/', include('users.urls'))
 ]
