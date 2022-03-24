23fbd72b08ed204c99757b078528cd3991c3b137
ouyang_meng <narelen@rambler.ru>
2022-03-24 22:54:33 +0300

    lesson 29 homework update

--- ads/models.py
+++ ads/models.py
@@ -18,9 +18,9 @@ class Ad(models.Model):
     name = models.CharField(max_length=50)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     price = models.PositiveIntegerField()
-    description = models.TextField(null=True)
+    description = models.TextField(null=True, blank=True)
     is_published = models.BooleanField(default=False)
-    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True)
     image = models.ImageField(upload_to="images/", null=True, blank=True)
 
     class Meta:
--- project/urls.py
+++ project/urls.py
@@ -1,22 +1,22 @@
-
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework import routers
 
-from ads import views
+from project import settings
+from users.views import LocationViewSet
+
+router = routers.SimpleRouter()
+router.register(r"location", LocationViewSet)
 
 urlpatterns = [
-    path('', views.root),
     path('admin/', admin.site.urls),
-    path('cat/', views.CategoryView.as_view()),
-    path('cat/<int:pk>', views.CategoryDetailView.as_view()),
-    path('cat/create/', views.CategoryCreateView.as_view()),
-    path('cat/<int:pk>/update/', views.CategoryUpdateView.as_view()),
-    path('cat/<int:pk>/delete/', views.CategoryDeleteView.as_view()),
-    path('ad/', views.AdView.as_view()),
-    path('ad/<int:pk>', views.AdDetailView.as_view()),
-    path('ad/create', views.AdCreateView.as_view()),
-    path('ad/<int:pk>/update/', views.AdUpdateView.as_view()),
-    path('ad/<int:pk>/delete', views.AdDeleteView.as_view()),
-    path('ad/<int:pk>/update_image', views.AdUploadImageView.as_view()),
-    path('user/', include('users.urls'))
+    path('api-auth/', include('rest_framework.urls')),
+    path('user/', include('users.urls')),
+    path('', include('ads.urls')),
 ]
+
+urlpatterns += router.urls
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
